bff15cb207731d8dd58c209a14eeeca898b0823d
Marwan Sabry <shobo12maro@hotmail.com>
2022-11-04 12:16:23 +0200

    minor formatting

--- app.js
+++ app.js
@@ -3,24 +3,20 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-// conenction to mongodb
-mongoose.connect("mongodb://localhost/todo_express", {
+// MongoDB Connection
+mongoose.connect("mongodb://localhost/item_input", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-
-// middlewares
+// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+// Routes
+app.use(require("./routes/index"));
+app.use(require("./routes/item"));
 
-
-// routes
-app.use(require("./routes/index"))
-app.use(require("./routes/todo"))
-
-
-// server configurations....
-app.listen(3000, () => console.log("Server started listening on port: 3000"));
+// Server Configs
+app.listen(3000, () => console.log("Server started listening on port: 3000."))
